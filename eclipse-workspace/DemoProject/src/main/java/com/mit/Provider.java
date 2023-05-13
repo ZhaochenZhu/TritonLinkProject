033d25d77cecd31001c93fedee2de11cc1ec7124
Zhaochen Zhu <83621307+ZhaochenZhu@users.noreply.github.com>
2023-05-12 17:19:46 -0700

    Delete Provider.java

--- eclipse-workspace/DemoProject/src/main/java/com/mit/Provider.java
+++
@@ -1,7 +0,0 @@
-package com.mit;
-
-public interface Provider {
-	String username = "postgres";
-	String pwd = "Zzc@20020623";
-	String connURL = "jdbc:postgresql://localhost:5432/TritonLink132B";
-}
